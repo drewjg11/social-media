df585550568fbb28051877a8ff1ac20239b7649a
John <drewjg11@gmail.com>
2021-08-16 13:47:15 -0500

    updates

--- simplesocial/simplesocial/urls.py
+++ simplesocial/simplesocial/urls.py
@@ -20,5 +20,5 @@ from .views import HomePage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', HomePage.as_view(), name='home')
+    path('', HomePage.as_view(), name='home'),
 ]
