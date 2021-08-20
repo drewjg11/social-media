0f01f4e21f0ddda67b2dad8da8d726f25d266754
John <drewjg11@gmail.com>
2021-08-20 09:01:03 -0500

    Part 6

--- simplesocial/projects/models.py
+++ simplesocial/projects/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.urls import reverse
+from django.conf import settings
 
+import misaka
+
+from orgs.models import Org
 # Create your models here.
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+class Project(models.Model):
+    user = models.ForeignKey(User, related_name='projects')
+    created_at = models.DateTimeField(auto_now=True)
+    item = models.TextField()
+    item_html = models.TextField(editable=False)
+    group = models.ForeignKey(Group, related_name='posts', null=True, blank=True)
+
+    def __str__(self):
+        return self.item
+
+    def save(self, *args, **kwargs):
+        self.item_html = misaka.html(self.item)
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('posts:single', kwargs={'username':self.user.username, 'pk':self.pk})
+
+    class Meta:
+        ordering = ['-created_at']
+        unique_together = ['user', 'message']
--- simplesocial/simplesocial/settings.py
+++ simplesocial/simplesocial/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
+    'orgs',
+    'projects',
     'bootstrap3'
 ]
 
